416e66491e1f9bcac6afad9eb4d28994afe30270
suosuo <fengfeng1037@163.com>
2018-07-23 15:19:16 +0800

    match after check

--- check.py
+++ check.py
@@ -55,14 +55,14 @@ class Checker:
         return self._result
     
     def dumps(self):
+        #保存到txt
         file = open('errorNum.txt','w')
         for path in self._result:
             print(path)
             result = self._result[path]
             for r in result:
                 if not r[1][0]:
-                    print(f"    |_{r[0]} match:{r[1][0]} file number:{r[1][1]}")
-                    #保存到txt
+                    print(f"    |_{r[0]} match:{r[1][0]} file number:{r[1][1]}") 
                     file.write(f"    |_{r[0]} match:{r[1][0]} file number:{r[1][1]}\n")
         file.flush()#结束之前将缓冲区内容添加到位
         file.close()
@@ -70,30 +70,30 @@ class Checker:
 
 def checkTheErrorFile(results):
     for key,valueList in results.items():#results.items(),字典不能直接遍历
-        print(f"dir:{key}")
+        #print(key)
+        keyDir = key.split('\\')
+        dir = keyDir[4]       
         for listItem in valueList:
             if listItem[1][0] == False:
                 nameOfError = listItem[0]
-                print(nameOfError)
+                yield dir,nameOfError #循环一次后return，再循环下一次，。yield后续用法与list相同
                 
-                yield key,nameOfError #循环一次后return，再循环下一次，。yield后续用法与list相同
 
-def matchTheErrorFile (originPath,string,errorPath,NameError):
-    text = open("nameErrorList.txt",'w')
-    text.write(errorPath)
-    errorPathBaseName = os.path.basename(errorPath)
-    print(errorPathBaseName)
-    for items in os.listdir(originPath):
-        if string in items:
-            subOriginPath = os.path.join(originPath,items)
-            for dir in os.listdir(subOriginPath):
-                for file in os.listdir(os.path.join(subOriginPath,dir)):
-                    str = file.split("_")
-                    print(str[-2])
-                    if errorPathBaseName == file:
-                        text.write(f"{str[-2]} name: {NameError}\n")
-                        print(f"{str[-2]} name: {NameError}\n")
+def matchTheErrorFile (originPath,string,errorDir,NameError):
+    text = open('nameErrorList.txt','w')
+    for file in os.listdir(originPath):
+        str = file.split("_")
+        if NameError == str[-1]:
+            print(f"origin Path:{originPath},  num:{str[-2]},  name: {NameError}")
+            text.write(f"origin Path:{originPath},  num:{str[-2]},  name: {NameError}\n")
+            
+    text.flush()
+    text.close()#使用text = open('.txt')打开后必须关闭text.close()，才能写入内容；使用with open则不需要
 
+def theFileTOcheck(path,string):
+    for dir in os.listdir(path):
+        if string in dir:
+            yield os.path.join(path,dir)
 
 if __name__ == "__main__":
     # pathAthlete = r"F:\201804athletes\athletes_data\collation_201804ath"  
@@ -112,17 +112,13 @@ if __name__ == "__main__":
     pathAthlete = r"F:\201804athletes\athletes_data\collation_201804ath"  
     pathHC = r"F:\201804athletes\athletes_data\collation_201804hc" 
     pathFile = ["3DT1","3PlT2FGRE","AssetCalibration","DTI","OAxT2FLAIR","rest","run1","run2","run3","run4","ScreenSave"]
-    numOfFiles = [159,18,120,4992,24,11438,4300,4300,4300,4300,3]
+    numOfFiles = [1,2,120,4992,24,11438,4300,4300,4300,4300,3]
     originPath = r"F:\201804athletes\athletes_data"
     string = "separate"
     checker = Checker(pathFile,numOfFiles)
     results = checker.check(pathAthlete).dumps().getResult()
-    for errorPath,NameError in checkTheErrorFile(results):
-        matchTheErrorFile(originPath,string,errorPath,NameError)
-        
-    
-    
-
-
-
-    
+    subOriginPath = theFileTOcheck(originPath,string)
+    for originItem in subOriginPath:
+        for errorPath,NameError in checkTheErrorFile(results):
+            lastPath = os.path.join(originItem,errorPath) 
+            matchTheErrorFile(lastPath,string,errorPath,NameError)
